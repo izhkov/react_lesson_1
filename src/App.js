0b22553432c9bd45f8a4b9825ad073f76e251be5
Ivan Izhkov <ivan.izhkov@mail.ru>
2023-05-09 16:03:35 +0500

    component with JSX

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// Декларативный
+import logo from "./logo.svg";
+import "./App.css";
+
+// Императивный
+let currentYear = new Date().getFullYear();
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<p>{currentYear}</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
